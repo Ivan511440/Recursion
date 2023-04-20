0df05f0261e6544686bdf229cfe7519f96660405
Ivan511440 <128604283+Ivan511440@users.noreply.github.com>
2023-04-20 20:00:35 +0600

    Zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте значения M и N.
+// Напишите программу, которая найдет сумму натуральных чисел в промежутке от M до N.
+
+// M = 1; N = 15 → 120
+// M = 4; N = 8 → 30
+
+using System;
+using static System.Console;
+
+Write("Введите число M: ");
+int m = Convert.ToInt32(ReadLine());
+
+Write("Введите число N: ");
+int n = Convert.ToInt32(ReadLine());
+
+SumFromMTon(m, n);
+
+void SumFromMTon(int m, int n)
+{
+    Write(SumMN(m - 1, n));
+}
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+    return 0;
+    else
+    {
+        m ++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+
+
